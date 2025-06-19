3fd59dc24161e454112cfe5ca0bc13e2c22f87eb
uday1sai <103194969+uday1sai@users.noreply.github.com>
2025-06-19 13:58:42 +0530

    made responsive

--- src/components/users/Register.jsx
+++ src/components/users/Register.jsx
@@ -1,30 +1,41 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+ 
 const Register = () => {
+  let [formData,setFromData]=useState([])
+  let handleChange=(e)=>{
+    let{name,value}=e.target
+    setFromData({...formData,[name]:value})
+  }
+   
+  let handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log(formData);
+    
+  }
   return (
     <div className='bg-[#efefef] size-full flex justify-center items-center '>
-      <form action="" className='w-1/2 h-3/4 rounded-2xl bg-white shadow-2xl flex justify-center items-center flex-col gap-4 px-20 py-40 '>
-        <div className='w-full flex justify-center items-center '>
-          <h1 className='text-3xl'>Registraion Form</h1>
+      <form action="" className='w-1/2 h-3/4 rounded-2xl bg-white shadow-2xl flex items-center flex-col gap-4 px-20 py-40 max-sm:w-[80%]' onSubmit={handleSubmit}>
+        <div className='w-full flex justify-center items-center ' >
+          <h1 className='text-3xl max-lg:text-sm max-sm:text-sm'>Registraion Form</h1>
         </div>
 
-        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
-          <input type="text" name="" placeholder='Enter Name' className='w-full outline-none px-4 h-10'/>
+        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm' >
+          <input type="text" name="name" placeholder='Enter Name' className='w-full outline-none px-4 h-10' onChange={handleChange}/>
         </div>
 
-        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
-          <input type="text" name="" placeholder='Enter UserName'   className='w-full outline-none px-4 h-10'/>
+        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm' >
+          <input type="text" name="username" placeholder='Enter UserName'   className='w-full outline-none px-4 h-10'onChange={handleChange}/>
         </div>
 
-        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
-          <input type="email" name="" placeholder='Enter Email'  className='w-full outline-none px-4 h-10'/>
+        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm' >
+          <input type="email" name="email" placeholder='Enter Email'  className='w-full outline-none px-4 h-10'onChange={handleChange}/>
         </div>
 
-        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
-          <input type="password" name="" placeholder='Enter Password'  className='w-full outline-none px-4 h-10'/>
+        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm' >
+          <input type="password" name="password" placeholder='Enter Password'  className='w-full outline-none px-4 h-10'onChange={handleChange}/>
         </div>
 
-        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm'>
+        <div className='border-2 w-full flex justify-center items-center px-3 rounded-sm' >
           <input type="password" name="" placeholder='Re-Enter Password'  className='w-full outline-none px-4 h-10'/>
         </div>
 
